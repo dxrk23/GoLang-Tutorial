02fee52f865b7f2afee5597389e09a95fb298739
Dinmukhamed <d.yedil@astanait.edu.kz>
2021-01-04 17:57:53 +0600

    added flag start

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
@@ -8,12 +9,20 @@ import (
 
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("snipppet/create" , createSnippet)
 
-	//log.Println("Starting server on :4000")
-	err:= http.ListenAndServe(":4000", mux)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	
 	log.Fatal(err)
 }
