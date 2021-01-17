cbcae288e9c55ecca06a7367c140f49d09a9fc0a
Dinmukhamed <d.yedil@astanait.edu.kz>
2021-01-17 16:24:05 +0600

    Connected to db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type application struct {
@@ -21,6 +24,14 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	dsn, err := pgxpool.Connect(context.Background(), 	"user=web password=1234 host=localhost port=5432 dbname=snippetbox sslmode=disable pool_max_conns=10")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer dsn.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
@@ -33,6 +44,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
